19dd02cfc3b1b0a853b48d450d40e87cf718992a
Ezgi Bas <ezgibas@pop-os.localdomain>
2021-12-26 21:40:40 -0500

    "router error fixed, homepage renders instead of compendium"

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Compendium from "./pages/Compendium";
 import HomePage from "./pages/HomePage";
@@ -5,8 +6,8 @@ import HomePage from "./pages/HomePage";
 function App() {
   return (
     <Routes>
-      <Route exact path="/" render={(props) => <HomePage></HomePage>}></Route>
-      <Route path = "/compendium" render={(props) => <Compendium></Compendium>}></Route>
+      <Route exact path="/" element={<HomePage></HomePage>}></Route>
+      <Route path="/compendium" element={<Compendium></Compendium>}></Route>
     </Routes>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,17 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Router } from "react-router-dom";
+import { Router , Routes, Route } from "react-router-dom";
 import { createBrowserHistory } from "history";
 
 const history = createBrowserHistory();
 
 ReactDOM.render(
+  <Router history={history} location="/">
   <React.StrictMode>
-    <Router history={history}>
-    <App />
-    </Router>
-  </React.StrictMode>,
+      <Routes>
+        <Route path = "*" element={<App></App>}></Route>
+    </Routes>
+  </React.StrictMode>
+  </Router>,
   document.getElementById('root')
 );
 
