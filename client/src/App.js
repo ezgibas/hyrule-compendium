9095a7b040e459b7a25eb742c9b5dd612bc2e257
Ezgi Bas <ezgibas@pop-os.localdomain>
2021-12-29 20:27:30 -0500

    "started processing api responses"

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Route, Routes, Link } from "react-router-dom";
 import Compendium from "./pages/Compendium";
 import HomePage from "./pages/HomePage";
+import Creatures from "./pages/Creatures";
+import IndividualCreature from "./pages/Creatures/indivCreature";
 
 function App() {
   return (
     <div>
     <Routes>
       <Route path="/compendium" element={<Compendium/>}/>
+      <Route exact path="/creatures" element={<Creatures/>}/>
+      <Route path="/creatures/:creatureName" element={<IndividualCreature/>}/>
       <Route exact path="/" element={<HomePage></HomePage>}/>
       <Route element={<h1>not found</h1>}/>
     </Routes>
