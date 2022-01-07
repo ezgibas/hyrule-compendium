ecf30a515c48ab351829229d0d51befaee7c331d
Ezgi Bas <ezgibas@pop-os.localdomain>
2022-01-07 14:55:22 -0500

    "started with turning data into a JSX"

--- client/src/pages/Creatures/indivCreature.js
+++ client/src/pages/Creatures/indivCreature.js
@@ -1,22 +1,37 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { CircularProgress } from "@material-ui/core";
 
 function IndividualCreature(props) {
   const { creatureName } = useParams();
-  const [creatureData, setCreature] = useState();
-  fetch(`https://botw-compendium.herokuapp.com/api/v2/entry/${creatureName}`)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      setCreature(data.data);
-    });
-  var creature = JSON.stringify(creatureData); //don't know why this is necessary but creatureData is undefined if i don't do it
+  const [creatureData, setCreatureData] = useState();
+  fetch(`https://botw-compendium.herokuapp.com/api/v2/entry/${creatureName}`);
+  useEffect(() => {
+    axios
+      .get(`https://botw-compendium.herokuapp.com/api/v2/entry/${creatureName}`)
+      .then(function (response) {
+        const { data } = response;
+        setCreatureData(data.data);
+      });
+  }, []);
   console.log(creatureData);
-    //const createCreatureJSX(creature)
+
+  const generateCreatureJSX = (creature) => {
+    return (
+      <div>
+        <h1>{`Name: ${creature.name}`}</h1>
+        <h3>{`Category: ${creature.category}`}</h3>
+        <p1>{creature.description}</p1>
+      </div>
+    );
+  };
+
   return (
     <div>
-      <h1>{`This is the page for creature named ${creatureName}`}</h1>
+      {/* {creatureData === undefined && <CircularProgress />} */}
+      {creatureData !== undefined && creatureData && generateCreatureJSX(creatureData)}
+      {/* {creatureData === false && <h1> Creature not found</h1>} */}
     </div>
   );
 }
